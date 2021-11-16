55421fc3d87583d29f541e772dba7acadeee9ff8
vlevi96 <vlevi9609@gmail.com>
2021-11-16 15:32:10 +0100

    Keddi óra

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+/*window.addEventListener("load", function () {
     console.log("Az oldal betöltődött");
 });
 
@@ -130,3 +130,162 @@ theLastFunction("hello", function () {
         }
     }
 */
+
+console.log(myFirstGlobalVar);
+var myFirstGlobalVar = 0;
+console.log(myFirstGlobalVar);
+
+//console.log(mySecondGlobalVar);
+let mySecondGlobalVar = 1;
+console.log(mySecondGlobalVar);
+
+//console.log(myThirdGlobalVar);
+const myThirdGlobalVar = 2;
+console.log(myThirdGlobalVar);
+
+console.log(myFirstGlobalVar, mySecondGlobalVar, myThirdGlobalVar);
+
+(function () {
+    //var anotherFunctionVar=undefined;
+    console.log(myFirstGlobalVar, mySecondGlobalVar, myThirdGlobalVar);
+
+    var myFirstFunctionVar = 3;
+    let mySecondFunctionVar = 4;
+    const myThirdFunctionVar = 5;
+
+    console.log(myFirstFunctionVar, mySecondFunctionVar, myThirdFunctionVar);
+    (function () {
+        console.log(myFirstGlobalVar, mySecondGlobalVar, myThirdGlobalVar);
+        console.log(myFirstFunctionVar, mySecondFunctionVar, myThirdFunctionVar);
+        (function () {
+            console.log(myFirstGlobalVar, mySecondGlobalVar, myThirdGlobalVar);
+            console.log(myFirstFunctionVar, mySecondFunctionVar, myThirdFunctionVar);
+            var a = 6;
+            console.log(a);
+        }())
+    }())
+}())
+//console.log(myFirstFunctionVar, mySecondFunctionVar, myThirdFunctionVar);
+
+function f1() {
+    console.log(a);
+    var a = undefined;
+    console.log(a);
+    a = 7;
+    console.log(a);
+}
+f1();
+
+function f2() {
+    console.log(a);
+    var a;
+    console.log(a);
+    a = 8;
+    console.log(a);
+}
+f2();
+
+function f3() {
+    //console.log(a);
+    let a;
+    console.log(a);
+    a = 8;
+    console.log(a);
+}
+f3();
+
+function f4() {
+    //console.log(a);
+    const a = 9;
+    console.log(a);
+    //a = 10;
+    //console.log(a);
+}
+f4();
+
+function f5() {
+    let b;
+    console.log(b);
+    console.log(typeof b);
+    /*
+    let c=typeof b;
+    console.log(typeof c);
+    */
+}
+f5();
+
+function f6() {
+    d = 12;
+    console.log(d);
+    if (true) {
+        (function () {
+            var d = 11;
+            console.log(d);
+
+        }())
+    }
+    console.log(d);
+}
+f6();
+console.log(d);
+
+function f7() {
+
+    if (true) {
+        let e = 13;
+        console.log(e);
+    }
+    //console.log(e);
+}
+f7();
+
+function f8() {
+    const ls = ["a", "b", "c", "d", "e", "f", true];
+
+    for (let i = 0; i < ls.length; i++) {
+        const item = ls[i];
+        console.log(item);
+    }
+    //console.log(i);
+
+    for (const item of ls) {
+        console.log(item);
+
+    }
+
+    for (const key in ls) {
+        const item = ls[key];
+        console.log(item);
+        console.log(typeof key);
+    }
+    console.log(f9(ls));
+}
+f8();
+
+function f9(arrayFromParam) {
+    console.log(arrayFromParam);
+
+    let abc = "";
+    for (const item of arrayFromParam) {
+        if (item !== true) {
+            abc += item;
+        }
+    }
+    //console.log(abc);
+    return abc;
+}
+
+//arrow
+/*const f10=(text)=>{
+    return `<div>${text}</div>`;
+};*/
+
+const f10 = (text) => `<div>${text}</div>`;
+
+console.log(f10("welcome"));
+
+window.addEventListener("load", function () {
+    const f9Result = f9(["hello", "bye"]);
+    document.getElementById("root").insertAdjacentHTML("afterbegin", f9Result);
+    document.getElementById("root").insertAdjacentHTML("afterbegin", f10("goodnight"));
+})
